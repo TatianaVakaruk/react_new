aec0321388194c13e938597cee4d1b4f3ff3b2c8
Tatiana <tatiana.vakaruk1@gmail.com>
2025-10-04 00:22:44 +0300

    First commit

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './search.scss';
+import Greetings from './Search';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Greeting name="Bob" />, rootElement);
