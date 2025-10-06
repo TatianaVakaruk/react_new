edbb6149c31808bf3c17360e425e8e67fea9850a
Tatiana <tatiana.vakaruk1@gmail.com>
2025-10-07 01:45:51 +0300

    First commit

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Greeting = (props) => {
+  const now = new Date();
+
+  let years = now.getFullYear() - props.birthDate.getFullYear();
+
+  // Учитываем месяц и день, чтобы результат был точным
+  const hasNotHadBirthdayThisYear =
+    now.getMonth() < props.birthDate.getMonth() ||
+    (now.getMonth() === props.birthDate.getMonth() &&
+      now.getDate() < props.birthDate.getDate());
+
+  if (hasNotHadBirthdayThisYear) {
+    years--;
+  }
+
   return (
-    <div className="greeting">{`My name is ${props.user.firstName} ${
-      props.user.lastName
-    }. I am ${props.user.getYearDifference()} years old`}</div>
+    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${years} years old`}</div>
   );
 };
 export default Greeting;
--- lesson3/task4/src/index.jsx
+++ lesson3/task4/src/index.jsx
@@ -2,29 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Greeting from './Greeting';
-const userData = {
-  firstName: 'John',
-  lastName: 'Doe',
-  birthDate: new Date('1985-10-07'),
-
-  getYearDifference() {
-    const now = new Date();
-
-    let years = now.getFullYear() - this.birthDate.getFullYear();
-
-    // Учитываем месяц и день, чтобы результат был точным
-    const hasNotHadBirthdayThisYear =
-      now.getMonth() < this.birthDate.getMonth() ||
-      (now.getMonth() === this.birthDate.getMonth() &&
-        now.getDate() < this.birthDate.getDate());
-
-    if (hasNotHadBirthdayThisYear) {
-      years--;
-    }
-    return years;
-  },
-};
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Greeting user={userData} />, rootElement);
+ReactDOM.render(
+  <Greeting
+    firstName="Tom"
+    lastName="Semuel"
+    birthDate={new Date('1985-10-08')}
+  />,
+  rootElement
+);
