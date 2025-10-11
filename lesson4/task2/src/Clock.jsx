54a1c98904f59c06d150a83986fe44f891a6a1d8
Tatiana <tatiana.vakaruk1@gmail.com>
2025-10-12 02:48:30 +0300

    Описание коммита

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -1,53 +1,26 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './clock.scss';
 
+const getTimeWithOffset = (offset) => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(
+    currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+  );
+};
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      date: getTimeWithOffset(this.props.offset),
       location: this.props.location,
-      offset: this.props.offset,
-      time: '',
     };
-  }
 
-  getTimeWithOffsetFormatted() {
-    let currentTime = new Date();
-
-    // Создаем новый объект Date и корректируем по сдвигу
-    let adjustedTime = new Date(currentTime);
-    adjustedTime.setHours(currentTime.getHours() + this.props.offset);
-
-    // Получаем часы, минуты, секунды
-    let hours = adjustedTime.getHours();
-    let minutes = adjustedTime.getMinutes();
-    let seconds = adjustedTime.getSeconds();
-
-    // Определяем AM или PM
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-
-    // Приводим часы к 12-часовому формату
-    hours = hours % 12;
-    hours = hours ? hours : 12; // 0 превращается в 12
-
-    // Форматируем с добавлением ведущих нулей
-    let strHours = hours.toString();
-    let strMinutes = minutes.toString().padStart(2, '0');
-    let strSeconds = seconds.toString().padStart(2, '0');
-
-    // Собираем финальную строку
-    return setInterval(() => {
-      if (strSeconds === 60) {
-        strSeconds = 0;
-        strMinutes += 1;
-      }
-      if (strMinutes === 60) {
-        strMinutes = 0;
-        strHours += 1;
-      }
-
+    setInterval(() => {
       this.setState({
-        time: `${strHours}:${strMinutes}:${strSeconds + 1} ${ampm}`,
+        date: getTimeWithOffset(this.props.offset),
+        location: this.props.location,
       });
     }, 1000);
   }
@@ -55,13 +28,12 @@ class Clock extends Component {
   render() {
     return (
       <div className="clock">
-        <div className="clock__location">{this.props.location}</div>
+        <div className="clock__location">{this.state.location}</div>
         <div className="clock__time">
-          {this.getTimeWithOffsetFormatted().toLocaleString()}
+          {moment(this.state.date).format('h:mm:ss A')}
         </div>
       </div>
     );
   }
 }
-
 export default Clock;
