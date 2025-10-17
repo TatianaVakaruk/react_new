e620d45f8f234013f5cdec56f431dc824b843da1
Tatiana <tatiana.vakaruk1@gmail.com>
2025-10-17 23:13:15 +0300

    Ваше сообщение коммита

--- lesson6/task3/src/Info.jsx
+++ lesson6/task3/src/Info.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Message = ({ text }) => {
+const Info = ({ text }) => {
   if (!text) {
     return null;
   }
   return <div className="info">{text}</div>;
 };
-export default Message;
+export default Info;
