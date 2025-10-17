c83410ce5143d46ca8c095d9f7374080f7196b62
Tatiana <tatiana.vakaruk1@gmail.com>
2025-10-18 00:29:03 +0300

    Ваше сообщение коммита

---
+++ lesson6/task4/src/Status.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Online from './Online.jsx';
+import Offline from './Offline.jsx';
+
+const Status = (props) => {
+  if (props.isOnline) {
+    return <Online />;
+  }
+  return <Offline />;
+};
+export default Status;
