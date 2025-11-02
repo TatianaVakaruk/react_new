318b016f82c603f5fa31f481d87b51d7fd7b8402
Tatiana <tatiana.vakaruk1@gmail.com>
2025-11-03 00:38:46 +0200

    First commit

---
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class ConnectionStatus extends Component {
+  state = {
+    status: 'online',
+  };
+  componentDidMount() {
+    window.addEventListener('online', this.isOnline);
+    window.addEventListener('offline', this.isOffline);
+  }
+
+  isOnline = () => {
+    this.setState({
+      status: 'online',
+    });
+  };
+  isOffline = () => {
+    this.setState({
+      status: 'offline',
+    });
+  };
+  componentWillUnmount() {
+    window.removeEventListener(`online`, this.isOnline());
+    window.removeEventListener(`offline`, this.isOffline());
+  }
+  render() {
+    return (
+      <div
+        className={`status ${
+          this.state.status === 'offline' ? ' status_offline' : ''
+        }`}
+      >
+        {this.state.status}
+      </div>
+    );
+  }
+}
+export default ConnectionStatus;
